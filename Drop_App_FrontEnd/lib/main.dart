7cf68b2b86c6da4b5de3059f8fd43eb814714ad0
Bami <pjbrahmi@kaist.ac.kr>
2024-11-11 17:22:49 +0900

    NavBar and Individual Requests

--- Drop_App_FrontEnd/lib/main.dart
+++ Drop_App_FrontEnd/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:drop_app/tabs/donate.dart';
+import 'package:drop_app/tabs/share.dart';
+import 'package:drop_app/tabs/chat.dart';
+import 'package:drop_app/tabs/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,41 +15,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Homepage App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+  
+  late TabController navbarcontroller;
+  
+  @override
+  void initState() {
+    super.initState();
+    navbarcontroller = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    navbarcontroller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome to My App'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, User!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20), // Spacer between text and button
-            ElevatedButton(
-              onPressed: () {
-                // Define button action here
-                print('Button Pressed');
-              },
-              child: Text('Get Started'),
-            ),
-          ],
+      body: TabBarView(
+        controller: navbarcontroller,
+        children: const <Widget> [Donate(), const Share(), Chat(), Profile()],
+      ),
+
+  bottomNavigationBar: Material(
+  elevation: 3,
+  child: Container(
+    height: 70, 
+    decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 249, 249, 249),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 1,
+          blurRadius: 5,
+          offset: const Offset(0, -3), // Shadow position (to add shadow above)
         ),
-      ),
+      ],
+    ),
+    child: TabBar(
+      tabs: <Tab>[
+        makeTab('assets/images/donate.png', 'Donate'),
+        makeTab('assets/images/share.png', 'Share'),
+        makeTab('assets/images/chat.png', 'Chat'),
+        makeTab('assets/images/profile.png', 'Profile')
+      ],
+      controller: navbarcontroller,
+    ),
+  ),
+),
     );
   }
+}
+
+Tab makeTab( String location, String tabName){
+  return Tab(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 28, 
+                height: 28,
+                child: Image.asset(location),
+              ),
+               Text(
+                tabName,
+                style: const TextStyle(
+                  fontSize: 12, 
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'Roboto', 
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        );
 }
---
+++ Drop_App_FrontEnd/lib/tabs/profile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        child: const Center(
+          child: Column(
+            // center the children
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.favorite,
+                size: 160.0,
+                color: Colors.white,
+              ),
+              Text(
+                "First Tab",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
